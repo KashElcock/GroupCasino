bcb8b2a85459fa1530fc26bac0b93b1197d8b6b3
KashElcock <116089696+KashElcock@users.noreply.github.com>
2022-11-16 17:25:01 -0500

    Player Class created, Player subclasses extended, Interfaces Implemented

--- src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGame.java
@@ -1,7 +1,20 @@
 package com.github.zipcodewilmington.casino.games.numberguess;
+import com.github.zipcodewilmington.casino.GameInterface;
+import com.github.zipcodewilmington.casino.PlayerInterface;
 
 /**
  * Created by leon on 7/21/2020.
  */
-public class NumberGuessGame {
+public class NumberGuessGame implements GameInterface {
+
+    public void add(PlayerInterface player) {
+
+    }
+
+    public void remove(PlayerInterface player) {
+
+    }
+    public void run() {}
+
+    public NumberGuessGame() {run();}
 }
--- src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessPlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessPlayer.java
@@ -1,7 +1,22 @@
 package com.github.zipcodewilmington.casino.games.numberguess;
+import com.github.zipcodewilmington.casino.CasinoAccountManager;
+import com.github.zipcodewilmington.casino.Player;
+import com.github.zipcodewilmington.casino.PlayerInterface;
 
 /**
  * Created by leon on 7/21/2020.
  */
-public class NumberGuessPlayer {
+public class NumberGuessPlayer extends Player implements PlayerInterface {
+
+    public CasinoAccountManager getAccount() {
+        return null;
+    }
+
+    public <SomeReturnType> SomeReturnType play() {
+        return null;
+    }
+
+    public NumberGuessPlayer() {
+        play();
+    }
 }
