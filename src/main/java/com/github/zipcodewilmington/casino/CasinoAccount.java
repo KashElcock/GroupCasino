302bd3da05d46dbc9a55280792e9ec198d1d48cc
Hanquan Liu <hanquanl0115@gmail.com>
2022-11-16 15:10:13 -0500

    casinoAccount test done

--- src/main/java/com/github/zipcodewilmington/casino/CasinoAccount.java
+++ src/main/java/com/github/zipcodewilmington/casino/CasinoAccount.java
@@ -5,11 +5,20 @@ package com.github.zipcodewilmington.casino;
  * `ArcadeAccount` is registered for each user of the `Arcade`.
  * The `ArcadeAccount` is used to log into the system to select a `Game` to play.
  */
-public class CasinoAccount {
 
-    private final String accountName;
-    private final String accountPassword;
-    public int accountBalance;
+/**
+ * Liu: I change all int to double
+ */
+public class CasinoAccount {
+    // test result: cannot assign a value to final variable
+    // private final String accountName;
+    private String accountName;
+    // test result: cannot assign a value to final variable
+    //private final String accountPassword;
+    private String accountPassword;
+    //Liu: I set 200 as default value of accountBalance for test purpose
+    public double accountBalance = 200.00;
+
 
     public CasinoAccount (String accountName, String accountPassword){
         this.accountName = accountName;
@@ -22,6 +31,10 @@ public class CasinoAccount {
         this.accountBalance = accountBalance;
     }
 
+    // A empty constructor
+    public CasinoAccount(){
+
+    }
 
     public String getAccountName() {
         return accountName;
@@ -31,19 +44,19 @@ public class CasinoAccount {
         return accountPassword;
     }
 
-    public int getAccountBalance() {
+    public double getAccountBalance() {
         return accountBalance;
     }
 
-    public void addAccountBalance(int amount){
+    public void addAccountBalance(double amount){
         accountBalance += amount;
     }
 
-    public void subtractAccountBalance(int amount){
+    public void subtractAccountBalance(double amount){
         accountBalance -= amount;
     }
 
-    public boolean checkAccountBalance(int amount){
+    public boolean checkAccountBalance(double amount){
         if (accountBalance >= amount) return true;
         else return false;
     }
--- src/test/java/com/github/zipcodewilmington/CasinoAccountTests.java
+++ src/test/java/com/github/zipcodewilmington/CasinoAccountTests.java
@@ -9,22 +9,89 @@ public class CasinoAccountTests {
     @Test
     public void casinoAccountContructorTest(){
         // given
+        String name = "test";
+        String password = "12345";
         // When
-        CasinoAccount casinoAccount = new CasinoAccount();
+        CasinoAccount casinoAccount = new CasinoAccount(name, password);
+
 
         //then
-        Assert.assertTrue(casinoAccount instanceof );
+        Assert.assertEquals(name, casinoAccount.getAccountName());
+        Assert.assertEquals(password, casinoAccount.getAccountPassword());
+
 
     }
 
     @Test
     public void casinoAccountNullaryContructotTest(){
+        //give
+        //when
         CasinoAccount casinoAccount = new CasinoAccount();
+
+        //then
+        Assert.assertTrue(casinoAccount instanceof CasinoAccount);
+
     }
 
     @Test
-    public void casinoAccountCheckBalanceTest(){
+    public void checkBalanceTest1(){
+        // given
+        // when
         CasinoAccount casinoAccount = new CasinoAccount();
+        casinoAccount.accountBalance = 200;
+        int amount = 100;
+        Assert.assertTrue(casinoAccount.checkAccountBalance(amount));
+
+    }
+    @Test
+    public void checkBalanceTest2(){
+        // given
+        // when
+        CasinoAccount casinoAccount = new CasinoAccount();
+        casinoAccount.accountBalance = 200;
+        int amount = 300;
+        Assert.assertFalse(casinoAccount.checkAccountBalance(amount));
+
+    }
+
+    @Test
+    public void getAccountBalanceTest(){
+        //give
+        CasinoAccount casinoAccount = new CasinoAccount();
+        double expectedAmount = 200;
+
+        //when
+        Assert.assertEquals(expectedAmount, casinoAccount.getAccountBalance() , 0.0f);
+
+    }
+
+    @Test
+    public  void addAccountBalanceTest(){
+        //give
+        CasinoAccount casinoAccount = new CasinoAccount();
+        double addedAmount = 100;
+        double expectedAmount = 300;
+
+        //when
+        casinoAccount.addAccountBalance(addedAmount);
+
+        //then
+        Assert.assertEquals(expectedAmount, casinoAccount.getAccountBalance(), 0.0f);
+    }
+
+    @Test
+    public  void subtractAccountBalanceTest(){
+        //give
+        CasinoAccount casinoAccount = new CasinoAccount();
+        double subtractedAmount = 100;
+        double expectedAmount = 100;
+
+        //when
+        casinoAccount.subtractAccountBalance(subtractedAmount);
+
+        //then
+        Assert.assertEquals(expectedAmount, casinoAccount.getAccountBalance(), 0.0f);
+
     }
 
 }
