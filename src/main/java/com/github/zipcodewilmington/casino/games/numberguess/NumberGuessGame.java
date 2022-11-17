14c9c3c42631d415016e0fff7ea9a751414997c2
KashElcock <116089696+KashElcock@users.noreply.github.com>
2022-11-17 13:50:40 -0500

    NumberGuess working

--- src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGame.java
@@ -14,7 +14,32 @@ public class NumberGuessGame implements GameInterface {
     public void remove(PlayerInterface player) {
 
     }
-    public void run() {}
+    public void run() {
+        int previousGuess = 0;
+        int numberOfTries = 0;
+        int mostRecentGuess = 0;
+        int mysteryNumber = GameInterface.getMysteryNumber();
+        System.out.println("Welcome to the Guessing Game Too Large Too Small");
+        // System.out.println(mysteryNumber);
+        while (mostRecentGuess != 100) {
+            mostRecentGuess = GameInterface.getIntegerInput("Please take a guess from 0 - 10 here: " + "\n Enter 100 to exit");
+            if (mostRecentGuess < 11) {
+                if (mostRecentGuess == previousGuess) {
+                    System.out.println("Didn't work last time, did it? Try Again!");
+                } else if (mostRecentGuess == mysteryNumber) {
+                    numberOfTries++;
+                    System.out.println("You Win! Correct guess :)");
+                } else if (mostRecentGuess < mysteryNumber) {
+                    numberOfTries++;
+                    System.out.println("Too Small, Try Again!");
+                } else if (mostRecentGuess > mysteryNumber) {
+                    numberOfTries++;
+                    System.out.println("Too Large, Try Again");
+                }
+            } previousGuess = mostRecentGuess;
+        }
+        System.out.println("\nIt took you " + numberOfTries + " tries!" + "\nEnding Application");
+    }
 
     public NumberGuessGame() {run();}
 }
