aeac78d421b77c2bba840c73893c6c3c7dcd9590
Hanquan Liu <hanquanl0115@gmail.com>
2022-11-18 08:53:40 -0500

    casino exception handling

--- src/main/java/com/github/zipcodewilmington/casino/games/highestrolldice/HighestRollDicePlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/highestrolldice/HighestRollDicePlayer.java
@@ -1,11 +1,11 @@
 package com.github.zipcodewilmington.casino.games.highestrolldice;
 
-import com.github.zipcodewilmington.casino.CasinoAccountManager;
+import com.github.zipcodewilmington.casino.CasinoAccount;
 import com.github.zipcodewilmington.casino.Player;
 import com.github.zipcodewilmington.casino.PlayerInterface;
 
 public class HighestRollDicePlayer extends Player implements PlayerInterface {
-    public CasinoAccountManager getAccount() {
+    public CasinoAccount getAccount() {
         return null;
     }
     public <SomeReturnType> SomeReturnType play() {
