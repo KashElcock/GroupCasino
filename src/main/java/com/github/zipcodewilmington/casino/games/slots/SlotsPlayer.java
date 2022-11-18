fda9cee83ad75a16713d1a5f4031265b0fd9ffa4
KashElcock <116089696+KashElcock@users.noreply.github.com>
2022-11-18 11:46:03 -0500

    account balance updates winning game money and is saving it from game to new game

--- src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsPlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsPlayer.java
@@ -12,7 +12,12 @@ public class SlotsPlayer extends Player implements PlayerInterface {
     public CasinoAccount getAccount() {
         return null;
     }
-    public <SomeReturnType> SomeReturnType play() {
+    public <SomeReturnType> Object play() {
         return null;
+    }
+
+    @Override
+    public double getResult(double result) {
+        return 0;
     }
 }
